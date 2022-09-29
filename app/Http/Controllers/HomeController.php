718e146b11f81e5d76adcc9bb14509c8037016c9
fcamacho <fcamacho@maas.es>
2022-09-29 22:56:38 +0200

    create templates

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $p1 = new \stdClass();
+        $p1->title = "Producto 1";
+        $p1->price = "60$";
+        $p2 = new \stdClass();
+        $p2->title = "Producto 2";
+        $p2->price = "40$";
+        return view('home',["product"=>[$p1,$p2]]);
+    }
+}
